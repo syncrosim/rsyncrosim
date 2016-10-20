3535c230bf3288714eab60545122ce650e92a77a
Josie Hughes <josie.s.hughes@gmail.com>
2016-10-20 16:54:05 -0400

    Initial documentation.

--- examples.R
+++ examples.R
@@ -3,6 +3,15 @@ library(devtools)
 devtools::load_all()
 #source("R/generics.R");source("R/ssimLibrary.R");source("R/session.R");source("R/command.R")
 
+?rsyncrosim
+?command
+?session
+?path
+?info
+?Session
+?SSimLibrary
+?ssimLibrary
+
 #Create a library called <model>.ssim in the current working directory.
 myLib = ssimLibrary(model="st-sim")
 session(myLib) #The SycroSim session
@@ -11,6 +20,7 @@ info(myLib) #Model type and other library information.
 
 #Open an existing SyncroSim library in the current working directory.
 myLib = ssimLibrary()
+myLib = ssimLibrary(name="st-sim",model="st-sim")
 
 #Create a library with name in the current working directory
 myLib2 = ssimLibrary(name="Lib2",model="st-sim")
@@ -21,6 +31,8 @@ myLib3 = ssimLibrary(name=paste0(getwd(),"/Temp/Lib3"),model="st-sim")
 #create or load a library using a specific session
 mySession = session("C:/Program Files/SyncroSim/1/SyncroSim.Console.exe")
 myLib = ssimLibrary(name="Lib2",cSession=mySession)
+
+
 
 showMethods(class="SSimLibrary")
 showMethods(class="Session")
