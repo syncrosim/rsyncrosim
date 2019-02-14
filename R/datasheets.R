c186681514f483e82d49291f2390e7e7375a7fa2
alex <alexe_webmail@yahoo.com>
2019-02-14 15:35:37 -0800

    Update date in file header blocks.

--- R/datasheets.R
+++ R/datasheets.R
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
+# Copyright (c) 2019 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
 # GPL v.3 License
 #' @include AAAClassDefinitions.R
 NULL
