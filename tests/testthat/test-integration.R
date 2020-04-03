5df2b0a8e371fd15d8d1d98f4f19f3c7b6b8bea9
VLucet <valentin.lucet@gmail.com>
2020-04-03 11:01:18 -0400

    fixes tests / outdated datasheet names conventions / different warnings

--- tests/testthat/test-integration.R
+++ tests/testthat/test-integration.R
@@ -8,7 +8,7 @@ setwd("./testLibs")
 
 test_that("Test simple non-spatial STSim example - assumes that SyncroSim is installed.", {
   skip_on_cran()
-
+  
   # Create the project definition
   libPath = paste0(getwd(),"/ST-Sim-Command-Line.ssim")
   ret=delete(libPath,force=T)
@@ -17,7 +17,7 @@ test_that("Test simple non-spatial STSim example - assumes that SyncroSim is ins
 
   #***********************************
   # Cover types and state classes
-  sheetName = "STSim_Stratum"; mySheet = datasheet(myProject,name=sheetName,empty=F,optional=T)
+  sheetName = "Stratum"; mySheet = datasheet(myProject,name=sheetName,empty=F,optional=T)
   mySheet[1,"Name"]="Entire Forest"
   mySheet[1,"Description"]="Another description"
   ret = saveDatasheet(myProject,mySheet,name=sheetName)
@@ -25,18 +25,20 @@ test_that("Test simple non-spatial STSim example - assumes that SyncroSim is ins
   expect_equal(datasheet(myProject,name=sheetName,empty=F,optional=F)$Description,"Another description") #returns optional columns and columns with data
   expect_equal(names(datasheet(myProject,name=sheetName,empty=F,optional=T)),c("Name","ID","Color","Legend","Description")) #returns all columns
 
-  sheetName = "STSim_StateClass"
-  expect_warning(datasheet(myProject,name=sheetName,empty=F),"StateLabelXID depends on STSim_StateLabelX. You should load STSim_StateLabelX before setting STSim_StateClass.")
+  sheetName = "StateClass"
+  expect_warning(datasheet(myProject,name=sheetName,empty=F),
+                 "StateLabelXID depends on stsim_StateLabelX. You should load stsim_StateLabelX before setting stsim_StateClass.", 
+                 "StateLabelYID depends on stsim_StateLabelY. You should load stsim_StateLabelY before setting stsim_StateClass.")
 
-  sheetName = "STSim_StateLabelY"
+  sheetName = "StateLabelY"
   mySheet = data.frame(Name="All")
   ret = saveDatasheet(myProject,mySheet,name=sheetName)
 
-  sheetName = "STSim_StateLabelX"
+  sheetName = "StateLabelX"
   mySheet = data.frame(Name=c('Coniferous','Deciduous','Mixed'))
   ret = saveDatasheet(myProject,mySheet,name=sheetName)
 
-  sheetName = "STSim_StateClass"; mySheet = datasheet(myProject,name=sheetName,empty=T)
+  sheetName = "StateClass"; mySheet = datasheet(myProject,name=sheetName,empty=T)
   expect_equal(levels(mySheet$StateLabelXID),c("Coniferous","Deciduous","Mixed"))
   mySheet[1:3,"StateLabelXID"]=levels(mySheet$StateLabelXID) #Valid values
   mySheet$StateLabelYID = levels(mySheet$StateLabelYID)[1] #Valid values
@@ -46,19 +48,19 @@ test_that("Test simple non-spatial STSim example - assumes that SyncroSim is ins
 
   #***********************************
   # Transitions
-  sheetName = "STSim_TransitionGroup"
+  sheetName = "TransitionGroup"
   mySheet = data.frame(Name = c("Fire","Harvest","Succession"))
   ret = saveDatasheet(myProject,mySheet,name=sheetName)
-  ret = saveDatasheet(myProject,mySheet,name="STSim_TransitionType")
+  ret = saveDatasheet(myProject,mySheet,name="TransitionType")
 
-  sheetName = "STSim_TransitionTypeGroup"
+  sheetName = "TransitionTypeGroup"
   mySheet = data.frame(TransitionTypeID = c("Fire","Harvest","Succession"))
   mySheet$TransitionGroupID = mySheet$TransitionTypeID
   ret = saveDatasheet(myProject,mySheet,name=sheetName)
 
   #****************
   # Age type
-  sheetName = "STSim_AgeType"
+  sheetName = "AgeType"
   mySheet = data.frame(Frequency=1,MaximumAge=100)
   ret = saveDatasheet(myProject,mySheet,name=sheetName)
 
@@ -69,13 +71,13 @@ test_that("Test simple non-spatial STSim example - assumes that SyncroSim is ins
 
   #**************
   # Run control
-  sheetName = "STSim_RunControl"
+  sheetName = "RunControl"
   mySheet = data.frame(MinimumIteration=1,MaximumIteration=2,MinimumTimestep=0,MaximumTimestep=10)
   ret = saveDatasheet(myScenario,mySheet,name=sheetName)
 
   #**************************
   # Deterministic transitions
-  sheetName = "STSim_DeterministicTransition"; mySheet = datasheet(myScenario,name=sheetName,optional=T,empty=T)
+  sheetName = "DeterministicTransition"; mySheet = datasheet(myScenario,name=sheetName,optional=T,empty=T)
   mySheet=addRow(mySheet,data.frame(StateClassIDSource="Coniferous:All",StateClassIDDest="Coniferous:All",AgeMin=21,Location="C1"))
   expect_equal(mySheet$Location,"C1")
   mySheet = addRow(mySheet,data.frame(StateClassIDSource="Deciduous:All",StateClassIDDest="Deciduous:All",Location="A1"))
@@ -86,7 +88,7 @@ test_that("Test simple non-spatial STSim example - assumes that SyncroSim is ins
 
   #*************************
   # Probabilistic transitions
-  sheetName = "STSim_Transition"
+  sheetName = "Transition"
   mySheet = data.frame(
     StateClassIDSource=c("Coniferous:All","Coniferous:All","Deciduous:All","Deciduous:All","Mixed:All","Mixed:All"),
     StateClassIDDest=c("Deciduous:All","Deciduous:All","Deciduous:All","Mixed:All","Coniferous:All","Deciduous:All"),
@@ -100,11 +102,11 @@ test_that("Test simple non-spatial STSim example - assumes that SyncroSim is ins
 
   #********************
   #Initial conditions
-  sheetName = "STSim_InitialConditionsNonSpatial"
+  sheetName = "InitialConditionsNonSpatial"
   mySheet = data.frame(TotalAmount=100,NumCells=100)
   ret=saveDatasheet(myScenario,mySheet,name=sheetName)
 
-  sheetName = "STSim_InitialConditionsNonSpatialDistribution"
+  sheetName = "InitialConditionsNonSpatialDistribution"
   mySheet=data.frame(
     StateClassID = c("Coniferous:All","Deciduous:All","Mixed:All"),
     AgeMin = c(20,NA,11),
@@ -127,7 +129,7 @@ test_that("Test simple non-spatial STSim example - assumes that SyncroSim is ins
 
   #******************
   # Transition targets
-  sheetName = "STSim_TransitionTarget"
+  sheetName = "TransitionTarget"
   mySheet=data.frame(TransitionGroupID = "Harvest",Amount=0)
   ret= saveDatasheet(myScenario,mySheet,name=sheetName)
 
@@ -138,7 +140,7 @@ test_that("Test simple non-spatial STSim example - assumes that SyncroSim is ins
 
   #******************
   # Transition targets
-  sheetName = "STSim_TransitionTarget"
+  sheetName = "TransitionTarget"
   mySheet=data.frame(TransitionGroupID = "Harvest",Amount=20)
   ret= saveDatasheet(myScenario,mySheet,name=sheetName)
 
@@ -157,7 +159,7 @@ test_that("Test simple non-spatial STSim example - assumes that SyncroSim is ins
   #********************************
   # See results
   #******************************
-  sheetName = "STSim_OutputStratumState"
+  sheetName = "OutputStratumState"
   mySQL = sqlStatement(groupBy=c("ScenarioID","Iteration","Timestep","StateLabelXID"),aggregate=c("Amount"))
   outStatesAllAges = datasheet(myResults,name=sheetName,sqlStatement=mySQL)
   expect_equal(setdiff(unique(outStatesAllAges$Timestep),seq(from=0,to=10)),numeric(0))
