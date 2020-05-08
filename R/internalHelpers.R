5aaa28a1e17c59dc09803c5f6cda5b2213445398
VLucet <valentin.lucet@gmail.com>
2020-05-08 18:36:20 -0400

    move standalone helper functions to the internal helpers

--- R/internalHelpers.R
+++ R/internalHelpers.R
@@ -170,3 +170,127 @@ printAndCapture <- function(x) {
   out <- strsplit(x, " ([", fixed = TRUE)[[1]][1]
   return(out)
 }
+
+# Dataframe from SyncroSim output
+#
+# Converts output from SyncroSim to a dataframe.
+#
+# @param x Output from \code{\link{command()}}
+# @param colNames A vector of column names.
+# @param csv If T assume comma separation. Otherwise, assume undefined white space separation.
+# @param localNames If T, remove spaces from column names and make camelCase.
+# @return A data frame of output from the SyncroSim console.
+# @examples
+# # Use a default session to create a new library
+# myArgs = list(list=NULL,columns=NULL,lib="C:/Temp/NewLibrary.ssim",sheet="stsim_Stratum",pid=1)
+# myOutput = command(args=myArgs,mySsim)
+# myDataframe = dataframeFromSSim(myOutput)
+# myDataframe
+#
+# Note: this function is now internal. Should now only be called from datasheet.
+
+.dataframeFromSSim <- function(x, colNames = NULL, csv = T, localNames = T, convertToLogical = NULL) {
+  if (is.null(colNames)) {
+    header <- T
+  } else {
+    header <- F
+  }
+  if (csv) {
+    con <- textConnection(x)
+    out <- read.csv(con, stringsAsFactors = F, header = header)
+    close(con)
+  } else {
+    if (1) {
+      # Do the old wierd thing if not csv
+      while (max(grepl("   ", x, fixed = T))) {
+        x <- gsub("   ", "  ", x, fixed = T)
+      }
+      x <- gsub("  ", ",", x, fixed = T)
+      con <- textConnection(x)
+      out <- read.csv(con, stringsAsFactors = F, header = header, sep = ",", encoding = "UTF-8")
+      if (!is.null(colNames)) {
+        lastName <- names(out)[length(names(out))]
+        if ((ncol(out) > length(colNames)) & (sum(!is.na(out[[lastName]])) == 0)) {
+          out[[lastName]] <- NULL
+        }
+        names(out) <- colNames
+      }
+    }
+    close(con)
+  }
+  if (localNames) {
+    names(out) <- gsub(" ", "", names(out))
+    names(out) <- gsub(".", "", names(out), fixed = T)
+    names(out) <- sapply(names(out), camel)
+  }
+  if (!is.null(convertToLogical)) {
+    for (i in seq(length.out = length(convertToLogical))) {
+      cName <- convertToLogical[[i]]
+      if (is.element(cName, names(out))) {
+        out[[cName]][out[[cName]] == "No"] <- F
+        out[[cName]][out[[cName]] == "Yes"] <- T
+        out[[cName]] <- as.logical(out[[cName]])
+      }
+    }
+  }
+  return(out)
+}
+
+# Gets datasheet summary info from an SsimLibrary, Project or Scenario.
+#
+# @details
+# See \code{\link{datasheet}} for discussion of optional/empty/sheetName/lookupsAsFactors arguments.
+# \itemize{
+#   \item {If x/project/scenario identify a scenario: }{Returns library, project, and scenario scope datasheets.}
+#   \item {If x/project/scenario identify a project (but not a scenario): }{Returns library and project scope datasheets.}
+#   \item {If x/project/scenario identify a library (but not a project or scenario): }{Returns library scope datasheets.}
+# }
+#
+# @param x An SsimLibrary, Project or Scenario object. Or a path to a SyncroSim library on disk.
+# @param project Project name or id. Ignored if x is a Project.
+# @param scenario Scenario name or id. Ignored if x is a Scenario.
+# @param scope "scenario","project", "library", "all", or NULL.
+# @param refresh If FALSE (default) names are retrieved from x@datasheetNames. If TRUE names are retrieved using a console call (slower).
+# @return A dataframe of datasheet names.
+# @examples
+#
+# Note: this function is now internal. Should now only be called from datasheet.
+
+datasheets <- function(x, project = NULL, scenario = NULL, scope = NULL, refresh = F) {
+  if (!is(x, "SsimObject")) {
+    stop("expecting SsimObject.")
+  }
+  
+  x <- .getFromXProjScn(x, project, scenario)
+  
+  # Get datasheet dataframe
+  if (!refresh) {
+    datasheets <- x@datasheetNames
+  } else {
+    tt <- command(c("list", "datasheets", "csv", paste0("lib=", .filepath(x))), .session(x))
+    datasheets <- .dataframeFromSSim(tt, convertToLogical = c("isOutput", "isSingle"))
+    datasheets$scope <- sapply(datasheets$scope, camel)
+    # TO DO - export this info from SyncroSim
+  }
+  datasheets$order <- seq(1, nrow(datasheets))
+  if (!is.null(scope) && (scope == "all")) {
+    datasheets$order <- NULL
+    return(datasheets)
+  }
+  if (is.element(class(x), c("Project", "SsimLibrary"))) {
+    datasheets <- subset(datasheets, scope != "scenario")
+  }
+  if (is.element(class(x), c("SsimLibrary"))) {
+    datasheets <- subset(datasheets, scope != "project")
+  }
+  if (!is.null(scope)) {
+    if (!is.element(scope, c("scenario", "project", "library"))) {
+      stop("Invalid scope ", scope, ". Valid scopes are 'scenario', 'project', 'library' and NULL.")
+    }
+    cScope <- scope
+    datasheets <- subset(datasheets, scope == cScope)
+  }
+  datasheets <- datasheets[order(datasheets$order), ]
+  datasheets$order <- NULL
+  return(datasheets)
+}
