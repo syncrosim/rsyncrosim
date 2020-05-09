5daf7745b8d9ff141777e9cbe290f10d42aa3c91
VLucet <valentin.lucet@gmail.com>
2020-05-09 15:51:32 -0400

    tests now use tempdir()

--- tests/testthat/test-unit.R
+++ tests/testthat/test-unit.R
@@ -1,10 +1,9 @@
 # Copyright (c) 2019 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
 # GPL v.3 License
 
-retDir <- getwd()
-unlink("testLibs", recursive = T)
-dir.create("testLibs")
-setwd("./testLibs")
+old_dir <- getwd()
+temp_dir <- tempdir()
+setwd(temp_dir)
 
 mySsim <- session()
 addPackage(session = mySsim, name = "stsimsf")
@@ -273,5 +272,5 @@ test_that("Tests of datasheet - assumes SyncroSim is installed", {
   ret <- delete(myLibrary, force = T)
 })
 
-setwd(retDir)
-unlink("testLibs", recursive = T)
+setwd(old_dir)
+unlink(temp_dir, recursive = T)
