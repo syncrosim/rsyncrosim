9f2a28da557dece7bee3af16666c5a74f9e66fc8
alex <alexe_webmail@yahoo.com>
2020-06-28 16:23:16 -0700

    Add autogentags function to package

---
+++ R/autogentags.R
@@ -0,0 +1,62 @@
+# Copyright (c) 2019 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
+# GPL v.3 License
+#' @include AAAClassDefinitions.R
+NULL
+
+#' Auto Generation Tags for a Scenario.
+#'
+#' Retrieves the Auto Generation Tags for a Scenario.
+#'
+#' @param ssimObject Scenario
+#' 
+#' @return 
+#' Returns the Auto Generation Tags
+#' @export
+setGeneric("autogentags", function(ssimObject) standardGeneric("autogentags"))
+
+#' @rdname autogentags
+setMethod("autogentags", signature(ssimObject = "character"), function(ssimObject) {
+  return(SyncroSimNotFound(ssimObject))
+})
+
+#' @rdname autogentags
+setMethod("autogentags", signature(ssimObject = "Scenario"), function(ssimObject) {
+  scnInfo <- scenario(ssimObject, summary = TRUE)
+  return(scnInfo$autoGenTags)
+})
+
+#' Auto Generation Tags for a Scenario.
+#'
+#' Sets the Auto Generation Tags for a Scenario.
+#'
+#' @param ssimObject Scenario
+#' @param tags character
+#' 
+#' @return 
+#' The updated ssimObject.
+#' 
+#' @export
+setGeneric("autogentags<-", function(ssimObject, value) standardGeneric("autogentags<-"))
+
+#' @rdname autogentags-set
+setReplaceMethod(
+  f = "autogentags",
+  signature = "character",
+  definition = function(ssimObject, value) {
+    return(ssimObject)
+  }
+)
+
+#' @rdname autogentags-set
+setReplaceMethod(
+  f = "autogentags",
+  signature = "Scenario",
+  definition = function(ssimObject, value) {
+    args <- list(setprop = NULL, lib = .filepath(ssimObject), autogentags = value, sid = .scenarioId(ssimObject))
+    tt <- command(args, .session(ssimObject))
+    if (!identical(tt, "saved")) {
+      stop(tt)
+    }
+    return(ssimObject)
+  }
+)
