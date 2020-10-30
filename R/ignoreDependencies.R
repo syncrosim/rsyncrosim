89dffb89b749739de9416499eed953470b753c5c
alex <alexe_webmail@yahoo.com>
2020-10-30 14:30:40 -0700

    Added new ignoreDependencies function.

---
+++ R/ignoreDependencies.R
@@ -0,0 +1,66 @@
+# Copyright (c) 2019 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
+# GPL v.3 License
+#' @include AAAClassDefinitions.R
+NULL
+
+#' Ignore Dependencies for a Scenario.
+#'
+#' Retrieves the Ignore Dependencies Datafeeds for a Scenario.
+#'
+#' @param ssimObject Scenario
+#' 
+#' @return 
+#' Returns Ignore Dependencies Datafeeds
+#' @export
+setGeneric("ignoreDependencies", function(ssimObject) standardGeneric("ignoreDependencies"))
+
+#' @rdname ignoreDependencies
+setMethod("ignoreDependencies", signature(ssimObject = "character"), function(ssimObject) {
+  return(SyncroSimNotFound(ssimObject))
+})
+
+#' @rdname ignoreDependencies
+setMethod("ignoreDependencies", signature(ssimObject = "Scenario"), function(ssimObject) {
+  scnInfo <- scenario(ssimObject, summary = TRUE)
+  return(scnInfo$ignoreDependencies)
+})
+
+#' Ignore Dependencies Datafeeds for a Scenario.
+#'
+#' Sets the Ignore Dependencies Datafeed for a Scenario.
+#'
+#' @param ssimObject Scenario
+#' @param dataFeeds character
+#' 
+#' @return 
+#' The updated ssimObject.
+#' 
+#' @export
+setGeneric("ignoreDependencies<-", function(ssimObject, value) standardGeneric("ignoreDependencies<-"))
+
+#' @rdname ignoreDependencies-set
+setReplaceMethod(
+  f = "ignoreDependencies",
+  signature = "character",
+  definition = function(ssimObject, value) {
+    return(ssimObject)
+  }
+)
+
+#' @rdname ignoreDependencies-set
+setReplaceMethod(
+  f = "ignoreDependencies",
+  signature = "Scenario",
+  definition = function(ssimObject, value) {
+    enquoted = NULL
+    if (!is.null(value) && value != ""){
+      enquoted = paste0('"', value, '"')
+    }
+    args <- list(setprop = NULL, lib = .filepath(ssimObject), ignoredeps = enquoted, sid = .scenarioId(ssimObject))
+    tt <- command(args, .session(ssimObject))
+    if (!identical(tt, "saved")) {
+      stop(tt)
+    }
+    return(ssimObject)
+  }
+)
