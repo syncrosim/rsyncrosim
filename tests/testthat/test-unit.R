871ead011850f15ae0d68ffd31fdc1bbc966aedb
val <valentin.lucet@gmail.com>
2021-02-11 18:59:41 +0000

    add comment to test file

--- tests/testthat/test-unit.R
+++ tests/testthat/test-unit.R
@@ -1,5 +1,5 @@
 # Copyright (c) 2019 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
-# GPL v.3 License
+# GPL v.3 License ## lines 41, 175, 226 fail in test env. 
 
 old_dir <- getwd()
 temp_dir <- tempdir()
