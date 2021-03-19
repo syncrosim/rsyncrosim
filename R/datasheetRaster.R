e17bccae15fbaac71727a45aa0d8a25b6459b6d3
Valentin <valentin.lucet@gmail.com>
2021-03-19 12:37:20 -0400

    improve datasheetRaster

--- R/datasheetRaster.R
+++ R/datasheetRaster.R
@@ -6,24 +6,36 @@ NULL
 #' Get spatial inputs or outputs from a Scenario(s).
 #'
 #' Get spatial inputs or outputs from one or more SyncroSim \code{\link{Scenario}}.
-#' @details
 #'
-#'
-#' The names of the returned raster stack contain metadata.
-#' For datasheets without Filename this is: paste0(<datasheet name>,".Scn",<scenario id>,".",<tif name>)
-#' For datasheets containing Filename this is: paste0(<datasheet name>,".Scn",<scenario id>,".It",<iteration>,".Ts",<timestep>)
-#'
-#' @param ssimObject SsimLibrary/Project/Scenario or list of Scenarios. If SsimLibrary/Project, then scenario argument is required.
-#' @param datasheet character string. The name of the datasheet containing the raster data.
-#' @param column character string. The name of the column in the datasheet containing the filenames for raster data. If NULL then use the first column that contains raster filenames.
-#' @param scenario character string, integer, or vector of these. The scenarios to include. Required if ssimObject is an SsimLibrary/Project, ignored if ssimObject is a list of Scenarios.
-#' @param iteration integer, character string, or vector of integer/character strings. Iteration(s) to include. If NULL then all iterations are included. If no Iteration column in the datasheet, then ignored.
-#' @param timestep integer, character string, or vector of integer/character string. Timestep(s) to include. If NULL then all timesteps are included.  If no Timestep column in the datasheet, then ignored.
-#' @param subset logical expression indicating datasheet rows to return. e.g. expression(grepl("Ts0001",Filename,fixed=T)). See subset() for details.
-#' @param forceElements logical. If TRUE then returns a single raster as a RasterStack; otherwise returns a single raster as a RasterLayer directly.
+#' @param ssimObject SsimLibrary/Project/Scenario or list of Scenarios. If 
+#'     SsimLibrary/Project, then scenario argument is required.
+#' @param datasheet character string. The name of the datasheet containing the 
+#'     raster data.
+#' @param column character string. The name of the column in the datasheet containing 
+#'     the filenames for raster data. If NULL then use the first column that contains 
+#'     raster filenames.
+#' @param scenario character string, integer, or vector of these. The scenarios to 
+#'     include. Required if ssimObject is an SsimLibrary/Project, ignored if 
+#'     ssimObject is a list of Scenarios.
+#' @param iteration integer, character string, or vector of integer/character strings. 
+#'     Iteration(s) to include. If NULL then all iterations are included. If no 
+#'     Iteration column in the datasheet, then ignored.
+#' @param timestep integer, character string, or vector of integer/character string. 
+#'     Timestep(s) to include. If NULL then all timesteps are included.  If no 
+#'     Timestep column in the datasheet, then ignored.
+#' @param subset logical expression indicating datasheet rows to return. e.g. 
+#'     expression(grepl("Ts0001",Filename,fixed=T)). See subset() for details.
+#' @param forceElements logical. If TRUE then returns a single raster as a RasterStack; 
+#'     otherwise returns a single raster as a RasterLayer directly.
 #' 
 #' @return 
 #' A RasterLayer, RasterStack or RasterBrick object. See raster package documentation for details.
+#' 
+#' @details 
+#' The names of the returned raster stack contain metadata.
+#' For datasheets without Filename this is: \code{paste0(<datasheet name>,".Scn",<scenario id>,
+#' ".",<tif name>)}. For datasheets containing Filename this is: 
+#' \code{paste0(<datasheet name>,".Scn",<scenario id>,".It",<iteration>,".Ts",<timestep>)}.
 #' 
 #' @examples
 #' \dontrun{
