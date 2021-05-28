0a750f216cb763204374515c9b595d413aeb6bf7
Valentin <valentin.lucet@gmail.com>
2021-05-28 10:39:22 -0400

    improve owner

--- R/owner.R
+++ R/owner.R
@@ -3,27 +3,55 @@
 #' @include AAAClassDefinitions.R
 NULL
 
-#' The owner of a SsimLibrary, Project or Scenario.
+#' Retrieves the owner of a SsimLibrary, Project or Scenario.
 #'
-#' Retrieves the owner of a \code{\link{SsimLibrary}}, \code{\link{Project}} or \code{\link{Scenario}}.
+#' Retrieves the owner of a \code{\link{SsimLibrary}}, \code{\link{Project}} or 
+#' \code{\link{Scenario}}.
 #'
-#' @param ssimObject SsimLibrary, Project or Scenario.
+#' @param ssimObject An object of class \code{\link{Session}}, \code{\link{Project}}, 
+#' or \code{\link{SsimLibrary}}.
 #' 
 #' @return 
 #' A character string: the owner of the ssimObject. 
+#' 
+#' @examples 
+#' \donttest{
+#' temp_dir <- tempdir()
+#' mySession <- session()
+#' myLibrary <- ssimLibrary(name = file.path(temp_dir,"testlib"), session = mySession)
+#' myProject <- project(myLibrary)
+#' myScenario <- scenario(myProject)
+#' 
+#' owner(myLibrary)
+#' owner(myProject)
+#' owner(myScenario)
+#' }
 #' 
 #' @export
 setGeneric("owner", function(ssimObject) standardGeneric("owner"))
 
 #' Set the owner of a SsimLibrary, Project or Scenario.
 #'
-#' Set the owner of a \code{\link{SsimLibrary}}, \code{\link{Project}} or \code{\link{Scenario}}.
+#' Set the owner of a \code{\link{SsimLibrary}}, \code{\link{Project}} or 
+#' \code{\link{Scenario}}.
 #'
-#' @param ssimObject Scenario, Project, or SsimLibrary.
-#' @param value The new owner.
+#' @param ssimObject An object of class \code{\link{Session}}, \code{\link{Project}}, 
+#' or \code{\link{SsimLibrary}}.
+#' @param value A character string, the new owner.
 #' 
 #' @return 
 #' The updated ssimObject.
+#' 
+#' @examples 
+#' \donttest{
+#' temp_dir <- tempdir()
+#' mySession <- session()
+#' myLibrary <- ssimLibrary(name = file.path(temp_dir,"testlib"), session = mySession)
+#' myProject <- project(myLibrary)
+#' myScenario <- scenario(myProject)
+#' 
+#' owner(myLibrary) <- "Apex RMS"
+#' }
 #' 
 #' @export
 setGeneric("owner<-", function(ssimObject, value) standardGeneric("owner<-"))
