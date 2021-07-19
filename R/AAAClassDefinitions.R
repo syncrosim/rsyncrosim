0e9eb9187ba2f7de4a61b21e5282ac871a71abe6
Katie Birchard <birchardkatie@gmail.com>
2021-07-19 11:10:30 -0500

    Updating copyright headers for all functions

--- R/AAAClassDefinitions.R
+++ R/AAAClassDefinitions.R
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
+# Copyright (c) 2021 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
 # GPL v.3 License
 
 setClassUnion("missingOrNULL", c("missing", "NULL"))
--- R/rsyncrosim.R
+++ R/rsyncrosim.R
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
+# Copyright (c) 2021 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
 # GPL v.3 License
 
 #' rsyncrosim: The R interface to SyncroSim: https://syncrosim.com/
