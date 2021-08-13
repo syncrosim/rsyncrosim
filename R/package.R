06a4591ae16c70a553aca99ffff86a31db855283
Katie Birchard <birchardkatie@gmail.com>
2021-08-13 11:46:46 -0700

    RSSIM-75: Added a list templates argument to package()

--- R/package.R
+++ R/package.R
@@ -13,9 +13,14 @@ NULL
 #' @param installed logical or character. \code{TRUE} (default) to list installed packages, 
 #' \code{FALSE} to list available packages, and "BASE" to list installed base 
 #' packages
+#' @param listTemplates character. Name of a SyncroSim package. If not \code{NULL} 
+#' (default), then lists all templates available for that package. The package
+#' must be installed in the current Session. Ignored if ssimObject is a 
+#' \code{\link{SsimLibrary}} object
 #' 
 #' @return 
-#' Returns a \code{data.frame} of packages installed.
+#' Returns a \code{data.frame} of packages installed or templates available 
+#' for a specified package.
 #' 
 #' @examples 
 #' \donttest{
@@ -37,50 +42,80 @@ NULL
 #'  
 #' # Check the package you're SsimLibrary is currently using
 #' package(myLibrary)
+#' 
+#' # Check the templates available for an installed package
+#' addPackage("helloworldEnhanced")
+#' package(listTemplates = "helloworldEnhanced")
 #' }
 #' 
 #' @export
-setGeneric("package", function(ssimObject = NULL, installed = TRUE) standardGeneric("package"))
+setGeneric("package", function(ssimObject = NULL, installed = TRUE, listTemplates = NULL) standardGeneric("package"))
 
 #' @rdname package
-setMethod("package", signature(ssimObject = "character"), function(ssimObject, installed = TRUE) {
+setMethod("package", signature(ssimObject = "character"), function(ssimObject, installed = TRUE, listTemplates) {
   return(SyncroSimNotFound(ssimObject, installed))
 })
 
 #' @rdname package
-setMethod("package", signature(ssimObject = "missingOrNULL"), function(ssimObject, installed = TRUE) {
+setMethod("package", signature(ssimObject = "missingOrNULL"), function(ssimObject, installed = TRUE, listTemplates) {
   ssimObject <- .session()
-  return(package(ssimObject, installed))
+  return(package(ssimObject, installed, listTemplates))
 })
 
 #' @rdname package
-setMethod("package", signature(ssimObject = "Session"), function(ssimObject, installed = TRUE) {
-  arg <- "installed"
-
-  if (installed == FALSE) {
-    arg <- "available"
-  }
+setMethod("package", signature(ssimObject = "Session"), function(ssimObject, installed = TRUE, listTemplates) {
+  if (is.null(listTemplates)) {
+    arg <- "installed"
   
-  if (installed == "BASE") {
-    arg <- "basepkgs"
-  }
-  
-  if (is.logical(installed)) {
-    tt <- command(c(arg), ssimObject, program = "SyncroSim.PackageManager.exe")
-    
-    if (tt[1] == "saved") {
-      out <- data.frame(name = NA, displayName = NA, version = NA)
-      out <- subset(out, !is.na(name))
-    } else if (grepl("The remote name could not be resolved", tt[1])) {
-      out <- "Could not connect to the package server."
-    } else {
-      out <- .dataframeFromSSim(tt, colNames = c("name", "description", "version"), csv = FALSE)
+    if (installed == FALSE) {
+      arg <- "available"
     }
-    return(out)
+    
+    if (installed == "BASE") {
+      arg <- "basepkgs"
+    }
+    
+    if (is.logical(installed)) {
+      tt <- command(c(arg), ssimObject, program = "SyncroSim.PackageManager.exe")
+      
+      if (tt[1] == "saved") {
+        out <- data.frame(name = NA, displayName = NA, version = NA)
+        out <- subset(out, !is.na(name))
+      } else if (grepl("The remote name could not be resolved", tt[1])) {
+        out <- "Could not connect to the package server."
+      } else {
+        out <- .dataframeFromSSim(tt, colNames = c("name", "description", "version"), csv = FALSE)
+      }
+      return(out)
+    } else {
+      tt <- command(c("list", arg, "csv"), ssimObject)
+      out <- .dataframeFromSSim(tt, localNames = TRUE, csv = FALSE)
+      return(out)
+    }
   } else {
-    tt <- command(c("list", arg, "csv"), ssimObject)
-    out <- .dataframeFromSSim(tt, localNames = TRUE, csv = FALSE)
-    return(out)
+    if (is.character(listTemplates)) {
+      
+      # Make sure package is installed
+      pkgList <- command(c("installed"), ssimObject,
+                         program = "SyncroSim.PackageManager.exe")
+      pkgDf <- .dataframeFromSSim(pkgList,
+                                  colNames = c("name", "description", "version"),
+                                  csv = FALSE)
+      if (listTemplates %in% pkgDf$name == FALSE) {
+        stop("SyncroSim package not installed")
+      }
+      
+      # Retrieve list of templates
+      args <- list(list = NULL, templates = NULL, noheaders = NULL,
+                   package = listTemplates)
+      tt <- command(args, program = "SyncroSim.Console.exe")
+      out <- .dataframeFromSSim(tt,
+                                colNames =c("name", "displayName", "installed"),
+                                csv = F)
+      return(out)
+    } else {
+      stop("listTemplates must be a character name of a SyncroSim Package")
+    }
   }
 })
 
