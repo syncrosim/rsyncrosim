dd93d5b6a514230b46ec7a2fcb41ff7de33811e7
Katie Birchard <33553358+katieb1@users.noreply.github.com>
2023-01-03 11:25:04 -0800

    Updating copyright for 2023

--- R/ssimEnvironment.R
+++ R/ssimEnvironment.R
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
+# Copyright (c) 2023 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
 # MIT License
 
 #' SyncroSim Environment
--- tests/testthat/test-unit.R
+++ tests/testthat/test-unit.R
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
+# Copyright (c) 2023 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
 # MIT License ## lines 41, 175, 226 fail in test env. 
 
 old_dir <- getwd()
