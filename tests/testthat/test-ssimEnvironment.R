8773f1336c11eb8ba07c47e3826aca21b97af398
Gabrielle Ednie <gabrielle.ednie@apexrms.com>
2023-02-14 13:26:21 -0500

    up to useConda

--- tests/testthat/test-ssimEnvironment.R
+++ tests/testthat/test-ssimEnvironment.R
@@ -10,56 +10,18 @@ test_that("ssimEnvironment works", {
   expect_s3_class(ssimEnvironment(), "data.frame")
 })
 
-test_that("runtimeInputFolder works", {
-  e <- ssimEnvironment()
-  e$InputDirectory <- tempdir()
-  myLibraryName <- file.path(e$TempDirectory, "empty")
-  myLibrary <- ssimLibrary(name = myLibraryName,
-                            session = mySession,
-                            package = "helloworldSpatial",
-                            template = "example-library",
-                            overwrite = TRUE)
+test_that("ssimEnvironment works", {
+  myLibraryName <- file.path(tempdir(),"empty")
+  mySession <- session()
+  myLibrary <- ssimLibrary(name = myLibraryName, session = mySession)
   myProject <- project(myLibrary, project = "Definitions")
   myScenario <- scenario(myProject, scenario = "My Scenario")
-  result <- run(myScenario)
 
-  expect_error(runtimeInputFolder(myScenario, datasheetName = "InputDatasheet"), NA)
-  expect_error(runtimeInputFolder(myScenario), NA)
-  expect_error(runtimeInputFolder(result), NA)
-})
-
-test_that("runtimeOutputFolder works", {
-  e <- ssimEnvironment()
-  e$TempDirectory <- tempdir()
-  myLibraryName <- file.path(e$TempDirectory, "empty")
-  myLibrary <- ssimLibrary(name = myLibraryName,
-                           session = mySession,
-                           package = "helloworldSpatial",
-                           template = "example-library",
-                           overwrite = TRUE)
-  myProject <- project(myLibrary, project = "Definitions")
-  myScenario <- scenario(myProject, scenario = "My Scenario")
-  result <- run(myScenario)
-
-  expect_error(runtimeOutputFolder(myScenario, datasheetName = "InputDatasheet"), NA)
-  expect_error(runtimeOutputFolder(result), NA)
-
-})
-
-test_that("runtimeTempFolder works", {
-  e <- ssimEnvironment()
-  e$TempDirectory <- tempdir()
-  myLibraryName <- file.path(e$TempDirectory, "empty")
-  myLibrary <- ssimLibrary(name = myLibraryName,
-                           session = mySession,
-                           package = "helloworldSpatial",
-                           template = "example-library",
-                           overwrite = TRUE)
-  myProject <- project(myLibrary, project = "Definitions")
-  myScenario <- scenario(myProject, scenario = "My Scenario")
-  result <- run(myScenario)
-
-  expect_error(runtimeTempFolder(tempdir()), NA)
-  expect_error(runtimeTempFolder(result), NA)
+  expect_error(ssimEnvironment(myScenario))
+  expect_error(ssimEnvironment(myLibrary))
+  expect_error(ssimEnvironment(myProject))
+  expect_error(ssimEnvironment(mySession))
+  expect_error(ssimEnvironment("myScenario"))
+  expect_error(ssimEnvironment(1))
 
 })
