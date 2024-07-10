98c6e2783729027078b77cdb74d9b61dd62d25cb
katieb1 <birchardkatie@gmail.com>
2024-07-10 14:50:37 -0700

    Remove template code from packages function

--- R/packages.R
+++ R/packages.R
@@ -14,10 +14,6 @@ NULL
 #' will be used
 #' @param installed logical or character. \code{TRUE} (default) to list 
 #' installed packages or \code{FALSE} to list available packages on the server
-#' @param listTemplates character. Name of a SyncroSim package. If not \code{NULL} 
-#' (default), then lists all templates available for that package. The package
-#' must be installed in the current Session. Ignored if ssimObject is a 
-#' \code{\link{SsimLibrary}} object
 #' 
 #' @return 
 #' Returns a \code{data.frame} of packages installed or templates available 
@@ -43,83 +39,61 @@ NULL
 #'  
 #' # Check the package(s) in your SsimLibrary
 #' packages(myLibrary)
-#' 
-#' # Check the templates available for an installed package
-#' installPackage("helloworldSpatial")
-#' packages(listTemplates = "helloworldSpatial")
 #' }
 #' 
 #' @export
-setGeneric("packages", function(ssimObject = NULL, installed = TRUE, listTemplates = NULL) standardGeneric("packages"))
+setGeneric("packages", 
+           function(ssimObject = NULL, installed = TRUE) standardGeneric("packages"))
 
 #' @rdname packages
-setMethod("packages", signature(ssimObject = "character"), function(ssimObject, installed = TRUE, listTemplates) {
+setMethod("packages", signature(ssimObject = "character"), 
+          function(ssimObject, installed = TRUE) {
   return(SyncroSimNotFound(ssimObject, installed))
 })
 
 #' @rdname packages
-setMethod("packages", signature(ssimObject = "missingOrNULL"), function(ssimObject, installed = TRUE, listTemplates) {
+setMethod("packages", signature(ssimObject = "missingOrNULL"), 
+          function(ssimObject, installed = TRUE) {
   ssimObject <- .session()
-  return(packages(ssimObject, installed, listTemplates))
+  return(packages(ssimObject, installed))
 })
 
 #' @rdname packages
-setMethod("packages", signature(ssimObject = "Session"), function(ssimObject, installed = TRUE, listTemplates) {
-  if (is.null(listTemplates)) {
-    arg <- "installed"
-  
+setMethod("packages", signature(ssimObject = "Session"), 
+          function(ssimObject, installed = TRUE) {
+            
     if (installed == FALSE) {
       arg <- "available"
+    } else {
+      arg <- "installed"
     }
     
     if (is.logical(installed)) {
+      
       tt <- command(c(arg), ssimObject, program = "SyncroSim.PackageManager.exe")
       
       if (tt[1] == "saved") {
-        # out <- data.frame(name = NA, displayName = NA, version = NA)
         out <- subset(out, !is.na(Name))
       } else if (grepl("The remote name could not be resolved", tt[1])) {
         out <- "Could not connect to the package server."
       } else {
         out <- .dataframeFromSSim(tt, localNames = TRUE, csv=FALSE)
       }
+      
       drops <- c("x")
       out <- out[ , !(names(out) %in% drops)]
+      
       return(out)
+      
     } else {
+      
       tt <- command(c("list", arg, "csv"), ssimObject)
       out <- .dataframeFromSSim(tt, localNames = TRUE, csv = FALSE)
       drops <- c("x")
       out <- out[ , !(names(out) %in% drops)]
+      
       return(out)
     }
-  } else {
-    if (is.character(listTemplates)) {
-      
-      # Make sure package is installed
-      pkgList <- command(c("installed"), ssimObject,
-                         program = "SyncroSim.PackageManager.exe")
-      pkgDf <- .dataframeFromSSim(pkgList,
-                                  localNames = TRUE,
-                                  csv = FALSE)
-      if (listTemplates %in% pkgDf$name == FALSE) {
-        stop("SyncroSim package not installed")
-      }
-      
-      # Retrieve list of templates
-      args <- list(list = NULL, templates = NULL, noheaders = NULL,
-                   package = listTemplates)
-      tt <- command(args, program = "SyncroSim.Console.exe")
-      out <- .dataframeFromSSim(tt,
-                                colNames =c("name", "displayName", "installed"),
-                                csv = F)
-      drops <- c("x")
-      out <- out[ , !(names(out) %in% drops)]
-      return(out)
-    } else {
-      stop("listTemplates must be a character name of a SyncroSim Package")
-    }
-  }
 })
 
 #' @rdname packages
