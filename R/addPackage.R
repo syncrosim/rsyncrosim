7a7dd2d4224c0899d9b29cae26babee3e5bd903e
katieb1 <birchardkatie@gmail.com>
2024-07-10 14:20:15 -0700

    Updated addPackage function to take version

--- R/addPackage.R
+++ R/addPackage.R
@@ -8,7 +8,9 @@ NULL
 #' Adds package(s) to a \code{\link{SsimLibrary}}.
 #'
 #' @param ssimLibrary \code{\link{SsimLibrary}} object
-#' @param name character string or vector of package name(s)
+#' @param packages character string or vector of package name(s)
+#' @param versions character string or vector of package version(s). If 
+#' \code{NULL} then uses the latest installed version of the package
 #' 
 #' @return
 #' Invisibly returns \code{TRUE} upon success (i.e.successful addition 
@@ -27,54 +29,78 @@ NULL
 #' 
 #' # Set up a SyncroSim Session, SsimLibrary, and Project
 #' mySession <- session()
-#' myLibrary <- ssimLibrary(name = myLibraryName, session = mySession, 
-#'                          package = "stsim")
+#' myLibrary <- ssimLibrary(name = myLibraryName, session = mySession)
 #' 
 #' # Add package
-#' addPackage(myLibrary, c("stsimsf"))
+#' addPackage(myLibrary, packages = "stsim", versions = "4.0.1")
+#' addPackage(myLibrary, packages = "stsimecodep")
 #' packages(myLibrary)
 #' 
 #' # Remove package
-#' removePackage(myLibrary, c("stsimsf"))
+#' removePackage(myLibrary, packages = c("stsim", "stsimecodep"))
 #' packages(myLibrary)
 #' }
 #' 
 #' @export
-setGeneric("addPackage", function(ssimLibrary, name) standardGeneric("addPackage"))
+setGeneric("addPackage", 
+           function(ssimLibrary, packages, versions) standardGeneric("addPackage"))
 
 #' @rdname addPackage
-setMethod("addPackage", signature(ssimLibrary = "character"), function(ssimLibrary, name) {
+setMethod("addPackage", signature(ssimLibrary = "character"), 
+          function(ssimLibrary, packages, versions) {
   return(SyncroSimNotFound(ssimLibrary))
 })
 
 #' @rdname addPackage
-setMethod("addPackage", signature(ssimLibrary = "SsimLibrary"), function(ssimLibrary, name) {
+setMethod("addPackage", signature(ssimLibrary = "SsimLibrary"), 
+          function(ssimLibrary, packages, versions) {
+  
+  # Check that list of packages and list of versions is same length
+  if (!is.null(versions) && (length(versions) != length(packages))){
+    stop("The number of versions supplied does not match the number of packages.")
+  }
+  
   sessionPkgs <- packages(.session(ssimLibrary))
   libraryPkgs <- packages(ssimLibrary)
   retList <- list()
-  for (i in seq(length.out = length(name))) {
-    cVal <- name[i]
-    if (is.element(cVal, libraryPkgs$name)){
-      print(paste0(cVal, " has already been added to the ssimLibrary"))
-      retList[[cVal]] <- FALSE
+  
+  for (i in seq(length.out = length(packages))) {
+    cPkg <- packages[i]
+    
+    cVer <- "0.0.0"
+    if (is.null(versions)){
+      pkgVersions <- sessionPkgs[sessionPkgs$name == cPkg]$version
+      if (nrow(pkgVersions) > 0){
+        cVer <- pkgVersions[length(pkgVersions)]
+      }
+    } else {
+      cVer <- versions[i]
+    }
+    
+    libPkgRow <- libraryPkgs[(libraryPkgs$name == cPkg) && (libraryPkgs$version == cVer), ]
+  
+    if (nrow(libPkgRow) > 0){
+      print(paste0(cPkg, " v", cVer, " has already been added to the ssimLibrary"))
+      retList[[cPkg]] <- FALSE
       next
     }
     
-    if (!is.element(cVal, sessionPkgs$name)) {
-      print(paste0("Warning - ", cVal, " is not among the available packages: ", 
+    if (!is.element(cPkg, sessionPkgs$name)) {
+      print(paste0("Warning - ", cPkg, " v", cVer, " is not among the available packages: ", 
                    paste(sessionPkgs$name, collapse = ",")))
-      retList[[cVal]] <- FALSE
+      retList[[cPkg]] <- FALSE
       next
     }
 
     tt <- command(list(add = NULL, package = NULL, lib = .filepath(ssimLibrary), 
-                       pkg = cVal), .session(ssimLibrary))
+                       pkg = cPkg, version = cVer), .session(ssimLibrary))
+    
     if (tt[1] == "saved"){
-      message(paste0("Package <", cVal, "> added"))
-      retList[[cVal]] <- TRUE
+      message(paste0("Package <", cPkg, " v", cVer, "> added"))
+      retList[[cPkg]] <- TRUE
     } else {
       message(tt)
-      retList[[cVal]] <- FALSE
+      retList[[cPkg]] <- FALSE
     }
   }
 
