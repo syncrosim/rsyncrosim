00249357a3280d6acdb45ee30f6e65fac06514f1
katieb1 <birchardkatie@gmail.com>
2025-01-22 14:36:01 -0800

    Fix examples for addPackage and removePackage

--- R/removePackage.R
+++ R/removePackage.R
@@ -20,7 +20,8 @@ NULL
 #' @examples
 #' \donttest{
 #' # Install "stsim" and "stsimecodep" SyncroSim packages
-#' installPackage("stsim")
+#' installPackage(packages = c("stsim", "stsim"),
+#'                versions = c("4.0.1", "4.3.5"))
 #' installPackage("stsimecodep")
 #' 
 #' # Specify file path and name of new SsimLibrary
