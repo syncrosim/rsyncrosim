614c6470ff739bb14e48beeba0db3eb106546e1f
katieb1 <birchardkatie@gmail.com>
2025-02-14 13:23:32 -0800

    Add profile function for retrieving SyncroSim profile info

---
+++ R/profile.R
@@ -0,0 +1,44 @@
+# Copyright (c) 2024 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
+# MIT License
+#' @include AAAClassDefinitions.R
+NULL
+
+#' Display SyncroSim profile
+#'
+#' Displays the currently signed in SyncroSim profile information. To sign in
+#' to SyncroSim use the \code{\link{signin}} function.
+#'
+#' @param session \code{\link{Session}} object. If \code{NULL}(default), the 
+#' default session will be used
+#' 
+#' @return 
+#' Character string: SyncroSim profile information.
+#' 
+#' @examples
+#' \dontrun{
+#' # Retrieve profile information for a SyncroSim session
+#' mySession <- session()
+#' profile(mySession)
+#' }
+#' 
+#' @export
+profile <- function(session = NULL) {
+  
+  # if a SyncroSim session is not provided, make one
+  if (is.null(session)) {
+    session <- .session()
+  }
+  if (is(session, "character") && (is(session, SyncroSimNotFound(warn = FALSE)))) {
+    return(SyncroSimNotFound())
+  }
+  
+  consoleName <- "SyncroSim.Console.exe"
+  sessionPath <- filepath(session)
+  
+  p <- processx::process$new(file.path(sessionPath, consoleName),
+                   args = c("--signin", "--profile"),
+                   stdin = "|", stdout = "|", stderr = "|")
+  Sys.sleep(1)
+  out <- p$read_output()
+  cat(out)
+}
