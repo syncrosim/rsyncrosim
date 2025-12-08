9d3c92e99037c5d1c766870aa5886f65feb1393b
Hannah Adams <67337692+hfadams@users.noreply.github.com>
2025-12-08 17:45:17 -0330

    updated comments to clarify cases that will reach metadata merging endpoint

--- R/datasheet.R
+++ R/datasheet.R
@@ -1094,7 +1094,8 @@ setMethod("datasheet",
         if (length(sid) == 1) {
             sheet$ScenarioId <- NULL
         }
-        # FIX: join by ScenarioId only
+        # NOTE: In the default single-scenario case, returnScenarioInfo is FALSE, so we do not merge scenario metadata
+        # The merge path below is primarily for multi-scenario outputs (or when returnScenarioInfo=TRUE)
         sheet <- merge(allScns, sheet, by = "ScenarioId", all.y = TRUE)
       }
     }
